fa0e5d50e9ff1d2eecdf06aed50017e8b926d4dd
ihidanaka <maks.kiselevk@yandex.ru>
2021-06-16 22:16:14 +0200

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,29 +1,12 @@
-const TYPES = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel'];
-const CHECKTIME = [
-  '12:00',
-  '13:00',
-  '14:00'];
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'];
-const PHOTOS_ADDRESSES = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TYPES = ['palace','flat','house','bungalow','hotel'];
+const CHECKTIMES = ['12:00','13:00','14:00'];
+const FEATURES = ['wifi','dishwasher','parking','washer','elevator','conditioner'];
+const PHOTOS_ADDRESSES = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 const MAXIMUM_PHOTOS_ARRAY_LENGTH = 16;
 
 const getRandomInteger = (min, max) => {
-  if (max === undefined || null) {
+  if ((max === undefined) || (max === null)) {
     max = Number.MAX_SAFE_INTEGER;
   }
 
@@ -32,62 +15,62 @@ const getRandomInteger = (min, max) => {
 
   }
 
-  if ((min || max) < 0) {
+  if ((min < 0) || (max < 0)) {
     throw new RangeError('На входе встречены отрицательные значения интервала');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-const getRandomFloat = (min, max, simbolsAmount) => {
+const getRandomFloat = (min, max, digit) => {
   if (min >= max) {
     throw new RangeError('Максимальное значение интервала должно быть больше минимального!');
   }
 
-  if ((min || max) < 0) {
+  if ((min < 0) || (max < 0)) {
     throw new RangeError('На входе встречены отрицательные значения интервала');
   }
-  return +((Math.random() * (max - min) + min).toFixed(simbolsAmount));
+  return +((Math.random() * (max - min) + min).toFixed(digit));
 };
-const getRandomAvatarAddress = (arrayElement) => (arrayElement < 10) ? `img/avatars/user0${arrayElement}.png` : `img/avatars/user${arrayElement}.png`;
-const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5);
+const getAuthorAvatar = (arrayElement) => (arrayElement < 10) ? `img/avatars/user0${arrayElement}.png` : `img/avatars/user${arrayElement}.png`;
+const shuffleArray = (arrays) => arrays.sort(() => Math.random() - 0.5);
 const getFeaturesArray = () => {
 
-  const featureShuffled = shuffleArray(FEATURES);
+  const shufflings = shuffleArray(FEATURES);
 
   const arrayAmount = getRandomInteger(0, FEATURES.length - 1);
 
-  const features = new Array(arrayAmount).fill(0);
-  features.forEach((val, ind) => {
-    features[ind] = featureShuffled[ind];
+  const features = new Array(arrayAmount).fill(null);
+  features.forEach((value, index) => {
+    features[index] = shufflings[index];
   });
 
   return features;
 };
 const getPhotosArray = (maxArrayLength) => {
-  const photosArray = new Array(getRandomInteger(1, maxArrayLength)).fill(0);
-  photosArray.forEach((val, ind) => {
-    photosArray[ind] = PHOTOS_ADDRESSES[getRandomInteger(0, PHOTOS_ADDRESSES.length - 1)];
+  const photos = new Array(getRandomInteger(1, maxArrayLength)).fill(null);
+  photos.forEach((value, index) => {
+    photos[index] = PHOTOS_ADDRESSES[getRandomInteger(0, PHOTOS_ADDRESSES.length - 1)];
   });
 
-  return photosArray;
+  return photos;
 };
-const createAdd = () => {
-  const LATITUDE = getRandomFloat(35.65000, 35.70000, 5);
-  const LONGTITUDE = getRandomFloat(139.70000, 139.80000, 5);
+const createAdvert = (index) => {
+  const LATITUDE = getRandomFloat(35.65000, 35.70000, 5); //Ругается es-lint если меняю на let
+  const LONGTITUDE = getRandomFloat(139.70000, 139.80000, 5); //Ругается es-lint если меняю на let
 
   return {
     author: {
-      avatar: getRandomAvatarAddress(),
+      avatar: getAuthorAvatar(index),
     },
 
     offer: {
       title: 'Аренда жилья в Токио',
-      address: `${LATITUDE},${LONGTITUDE}`,
-      price: getRandomInteger(0).toString(),
+      address: `${LATITUDE}, ${LONGTITUDE}`,
+      price: getRandomInteger(0),
       type: TYPES[getRandomInteger(0, TYPES.length - 1)],
-      rooms: getRandomInteger(0).toString(),
-      guests: getRandomInteger(0).toString(),
-      checkin: CHECKTIME[getRandomInteger(0, CHECKTIME.length - 1)],
-      checkout: CHECKTIME[getRandomInteger(0, CHECKTIME.length - 1)],
+      rooms: getRandomInteger(0),
+      guests: getRandomInteger(0),
+      checkin: CHECKTIMES[getRandomInteger(0, CHECKTIMES.length - 1)],
+      checkout: CHECKTIMES[getRandomInteger(0, CHECKTIMES.length - 1)],
       features: getFeaturesArray(),
       description: 'Внимание! Смарт-квартира в стиле лофт в центре города НЕ подойдет скучному нытику. Если это не о тебе, тогда барная стойка из натурального камня ждет тебя на пару коктейлей. А джакузи приглашает расслабиться перед вечеринкой с другом или подругой. Кстати, тебе даже не придется вызывать такси из клуба — все заведения в 500 метрах от дома',
       photos: getPhotosArray(MAXIMUM_PHOTOS_ARRAY_LENGTH),
@@ -99,18 +82,9 @@ const createAdd = () => {
     },
   };
 };
-const fillAds = (elementsAmount) => {
-  const adID = new Array(elementsAmount).fill(0);
-  adID.forEach((val, index) => adID[index] = index + 1);
-  const shuffledArray = shuffleArray(adID);
-
-  const AdsArray = new Array(elementsAmount).fill(0);
-  AdsArray.forEach((val, ind) => {
-    AdsArray[ind] = createAdd();
-    AdsArray[ind].author.avatar = getRandomAvatarAddress(shuffledArray[ind]);
-  });
-
-  return AdsArray;
+const getAdvertsArray = (count) => {
+  const adverts = new Array(count).fill(null).map((value, index) => createAdvert(index + 1));
+  return adverts;
 };
 
-fillAds(10);
+getAdvertsArray(10);
