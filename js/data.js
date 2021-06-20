c5b617b1cd602b09873346eb33512c1261d6b5c3
ihidanaka <maks.kiselevk@yandex.ru>
2021-06-20 14:54:01 +0200

    Modules

---
+++ js/data.js
@@ -0,0 +1,65 @@
+import {getRandomInteger,getRandomFloat,getAuthorAvatar,shuffleArray} from './util.js';
+
+const types = ['palace','flat','house','bungalow','hotel'];
+const checktimes = ['12:00','13:00','14:00'];
+const features = ['wifi','dishwasher','parking','washer','elevator','conditioner'];
+const photosAddresses = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const maxPhotosArrayLength = 16;
+
+const getFeatures = () => {
+
+  const shufflings = shuffleArray(features);
+
+  const arrayAmount = getRandomInteger(0, features.length - 1);
+
+  const featuresData = new Array(arrayAmount).fill(null);
+  featuresData.forEach((value, index) => {
+    featuresData[index] = shufflings[index];
+  });
+
+  return featuresData;
+};
+const generatePhotos = (count) => {
+  const photos = new Array(getRandomInteger(1, count)).fill(null);
+  photos.forEach((value, index) => {
+    photos[index] = photosAddresses[getRandomInteger(0, photosAddresses.length - 1)];
+  });
+
+  return photos;
+};
+const createAdvert = (id) => {
+  const latitude = getRandomFloat(35.65000, 35.70000, 5);
+  const longtitude = getRandomFloat(139.70000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: getAuthorAvatar(id),
+    },
+
+    offer: {
+      title: 'Аренда жилья в Токио',
+      address: `${latitude}, ${longtitude}`,
+      price: getRandomInteger(0),
+      type: types[getRandomInteger(0, types.length - 1)],
+      rooms: getRandomInteger(0),
+      guests: getRandomInteger(0),
+      checkin: checktimes[getRandomInteger(0, checktimes.length - 1)],
+      checkout: checktimes[getRandomInteger(0, checktimes.length - 1)],
+      features: getFeatures(),
+      description: 'Внимание! Смарт-квартира в стиле лофт в центре города НЕ подойдет скучному нытику. Если это не о тебе, тогда барная стойка из натурального камня ждет тебя на пару коктейлей. А джакузи приглашает расслабиться перед вечеринкой с другом или подругой. Кстати, тебе даже не придется вызывать такси из клуба — все заведения в 500 метрах от дома',
+      photos: generatePhotos(maxPhotosArrayLength),
+    },
+
+    location: {
+      lat: latitude,
+      lng: longtitude,
+    },
+  };
+};
+const generateAdverts = (count) => {
+  const adverts = new Array(count).fill(null).map((value, index) => createAdvert(index + 1));
+  return adverts;
+};
+
+export {getFeatures,generatePhotos,createAdvert,generateAdverts};
---
+++ js/util.js
@@ -0,0 +1,28 @@
+const getRandomInteger = (min, max = Number.MAX_SAFE_INTEGER) => {
+
+  if (min >= max) {
+    throw new RangeError('Максимальное значение интервала должно быть больше минимального!');
+
+  }
+
+  if ((min < 0) || (max < 0)) {
+    throw new RangeError('На входе встречены отрицательные значения интервала');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomFloat = (min, max, digit) => {
+  if (min >= max) {
+    throw new RangeError('Максимальное значение интервала должно быть больше минимального!');
+  }
+
+  if ((min < 0) || (max < 0)) {
+    throw new RangeError('На входе встречены отрицательные значения интервала');
+  }
+  return +((Math.random() * (max - min) + min).toFixed(digit));
+};
+
+const getAuthorAvatar = (id) => id < 10 ? `img/avatars/user0${id}.png` : `img/avatars/user${id}.png`;
+const shuffleArray = (items) => items.sort(() => Math.random() - 0.5);
+
+export {getRandomInteger,getRandomFloat,getAuthorAvatar,shuffleArray};
