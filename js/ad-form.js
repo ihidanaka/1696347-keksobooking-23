4e90a1dc2984f2a8c663caf7115ff32868ace351
ihidanaka <maks.kiselevk@yandex.ru>
2021-07-12 18:35:03 +0200

    8.1

---
+++ js/ad-form.js
@@ -0,0 +1,44 @@
+const typeSelectInput = document.querySelector('#type');
+const pricePerNightInput = document.querySelector('#price');
+const roomNumber = document.querySelector('#room_number');
+const roomCapacity = document.querySelector('#capacity')
+const capacityList = roomCapacity.querySelectorAll('option');
+
+const offerTypeToMinPrice = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+const roomNumberToGuests = {
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0],
+};
+const typeSelectInputChangeHandler = () => {
+  const minPrice = offerTypeToMinPrice[typeSelectInput.value];
+
+  pricePerNightInput.min = minPrice;
+  pricePerNightInput.placeholder = minPrice;
+};
+
+typeSelectInput.addEventListener('change', typeSelectInputChangeHandler);
+
+roomNumber.addEventListener('change', () => {
+  const guests = roomNumberToGuests[roomNumber.value];
+
+  capacityList.forEach((option) => {
+    const isValid = guests.includes(+option.value);
+
+
+    option.classList[isValid ? 'remove' : 'add']('hidden');
+  });
+
+  const errorMessage = guests.includes(+roomCapacity.value)
+    ? ''
+    : 'Укажите правильное кол-во мест!';
+
+  roomCapacity.setCustomValidity(errorMessage);
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {generateAdverts} from './data.js';
 import {addCard} from './map.js';
+import  './ad-form.js';
 
 const adverts = generateAdverts(10);
 
 addCard(adverts[0]);
+
